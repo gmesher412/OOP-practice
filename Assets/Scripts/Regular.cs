a3d8ddcf266b44e3f69b7956a824ac5451826f91
gmesher412 <g.mesher412@gmail.com>
2025-01-06 20:13:56 +0100

    Basic enemy commit
    - Made simple enemy and player prefabs
    - Added the enemy class and one subclass
    - Need to review how the different OOP principles work

---
+++ Assets/Scripts/Regular.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Regular : Enemy
+{
+    
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
