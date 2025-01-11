9accc2152b4d4396a213edd95d0e2fe89d2056f2
gmesher412 <g.mesher412@gmail.com>
2025-01-11 08:48:32 +0100

    Basic gameplay fully implemented
    - Added an invisible wall that prevents the player from falling off the map

--- Assets/Scripts/Units/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Units/Player Scripts/PlayerController.cs
@@ -4,12 +4,14 @@ public class PlayerController : Unit
 {
     [SerializeField] Vector3 moveValue;
     InputController inputController;
+    GameObject range;
     
     void Start()
     {
         inputController = GetComponent<InputController>();
         Health = 6;
-        UnitSpeed = 10;
+        UnitSpeed = 6;
+        range = GameObject.Find("Range");
     }
 
     void Update()
@@ -19,7 +21,6 @@ public class PlayerController : Unit
 
     void FixedUpdate()
     {
-
         UnitMovement(moveValue);
     }
 
@@ -29,6 +30,16 @@ public class PlayerController : Unit
         {
             int damage = other.gameObject.GetComponent<Enemy>().Damage;
             TakeDamage(damage);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == range)
+        {
+            Vector3 currentVelocity = unitRB.linearVelocity;
+            // unitRB.linearVelocity = Vector3.zero;
+            unitRB.AddForce(-currentVelocity, ForceMode.Impulse);
         }
     }
 }
